4a4b4ae68117a1f494e43ef331bc354dacfa4aa8
JIAHAO CHEN <100472232@alumnos.uc3m.es>
2025-11-06 23:35:55 -0500

    v 0.1

---
+++ script.js
@@ -0,0 +1,13 @@
+// Welcome message
+console.log("Welcome to Jiahao Chen's portfolio!");
+
+// Future JavaScript for interactivity can go here.
+// For example, a mobile menu toggle:
+/*
+const navToggle = document.querySelector('.nav-toggle'); // You would need to add this button in HTML
+const navLinks = document.querySelector('nav ul');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+*/
